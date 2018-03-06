55dfb32fd1e36731b53671c7612bd60d2f03bf4b
William Pinto <william.pinto@lambda3.com.br>
2018-03-06 13:50:01 -0300

    Criado projeto e alterado arquivo MessagesController

---
+++ TelesBot/TelesBot/Controllers/MessagesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace TelesBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            await HandleSystemActivity(activity);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+
+            return response;
+        }
+
+        private async Task HandleSystemActivity(Activity activity)
+        {
+            switch (activity.Type)
+            {
+                case ActivityTypes.ConversationUpdate:
+                    // Handle conversation state changes, like members being added and removed
+                    // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                    // Not available in all channels
+                    break;
+
+                case ActivityTypes.Message:
+                    await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                    break;
+
+                case ActivityTypes.DeleteUserData:
+                    // Implement user deletion here
+                    // If we handle user deletion, return a real message
+                    break;
+
+                case ActivityTypes.ContactRelationUpdate:
+                    // Handle add/remove from contact lists
+                    // Activity.From + Activity.Action represent what happened
+                    break;
+
+                case ActivityTypes.Typing:
+                    // Handle knowing tha the user is typing
+                    break;
+
+                case ActivityTypes.Ping:
+
+                    break;
+            }
+        }
+    }
+}
